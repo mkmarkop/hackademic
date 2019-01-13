5260edec563e71848b52a41a33911702779fd1b6
Marko Pranjic <marko.pranjic@fer.hr>
2019-01-13 23:32:45 +0100

    Fix DB date issue

--- installation/installer/data/database.php
+++ installation/installer/data/database.php
@@ -15,7 +15,7 @@
 	$query  = "INSERT INTO users (id,username, full_name, email, password, joined, last_visit,";
 	$query .= "is_activated ,type ,token) VALUES (0,'".$_SESSION['admin_username']."', ";
 	$query .= "'Administrator','".$_SESSION['admin_email']."','".$_SESSION['admin_password']."', ";
-	$query .= "'".date("Y-m-d H-i-s")."','0000-00-00 00:00:00', '1','1','0');";
+	$query .= "'".date("Y-m-d H-i-s")."','1970-01-01 08:00:00', '1','1','0');";
 	$SQL[] = $query;
 	$query = "INSERT INTO class_memberships (user_id,class_id) VALUES (0,1)";
 	$SQL[] = $query;
